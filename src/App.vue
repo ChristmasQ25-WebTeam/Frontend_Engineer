b4201d9d59b4e06dd44fce3b7eafae233f24a921
woominhee <maya2001bee@naver.com>
2022-07-30 22:52:00 +0900

    #10 #11 구현

--- src/App.vue
+++ src/App.vue
@@ -133,7 +133,7 @@
 
       <button @click="ClickButton =true" class="next_button">완료</button>
 
-      </div>
+  </div>
 
   <!-- 미니 : 회원가입 view-->
   <div v-if="signUp_page==true" id="signUp_page">
@@ -202,7 +202,7 @@
           </li>
         </ul>
         <p>confirm</p>
-        <img src="../src/assets/02_stamp.png" alt="스탬프" v-if="pwformOpen == false && pwOpen == false && emailOpen == false && nickOpen == false">
+        <img id="stamp_img" src="../src/assets/02_stamp.png" alt="스탬프" v-if="pwformOpen == false && pwOpen == false && emailOpen == false && nickOpen == false">
       </div>
       <div id="wrap3">
         <button class="finish-btn" @click="submit">완료</button>
@@ -211,8 +211,148 @@
     
   </div>
 
+  <!-- 미니 : 비번변경 view -->
+  <div v-if="changePw_page==true" id="changePw_page">
+    <div class="modal">
+      <div class="modal_background" v-if="newpwOpen == true">
+        <div class="modal_box">
+          <h4>비밀번호 형식을 확인해주세요</h4>
+          <button @click="check">확인</button>
+        </div>
+      </div>
+      <div class="modal_background check-oldpw" v-if="oldpwOpen == true">
+        <div class="modal_box">
+          <h4>기존 비밀번호가<br>일치하지 않습니다</h4>
+          <button @click="check">확인</button>
+        </div>
+      </div>
+      <div class="modal_background" v-if="changepwOpen == true">
+        <div class="modal_box">
+          <h4>비밀번호가 변경되었습니다</h4>
+          <button @click="togo_Qlist_page">확인</button>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <div id="wrap1">
+        <i class="material-icons">keyboard_arrow_left</i>
+      </div>
+      <div id="wrap2">
+        <h3>Change</h3>
+        <img id="changePw_img" src="../src/assets/04_key.png" alt="비번변경">
+        <ul>
+          <li>
+            <div class="label-box">
+              <span>기존의 비밀번호를 입력해주세요!</span>
+            </div>
+            <div class="input-box">
+              <input type="password" class="inputText" v-model="old_pw" placeholder="기존 비밀번호를 입력해주세요!">
+            </div>
+          </li>
+          <li>
+            <div class="label-box">
+              <span>새 비밀번호를 입력해주세요!</span>
+            </div>
+            <div class="input-box newPw-input">
+              <input type="password" class="inputText" v-model="new_pw" placeholder="새 비밀번호를 입력해주세요!" @change="chkInput_new">
+              <p :class="[chknewPw === false ? 'unchk' : 'chk']">*영문/숫자 포함 6자 이상</p>
+            </div>
+          </li>
+        </ul>
+        <p>confirm</p>
+        <img id="stamp_img" src="../src/assets/02_stamp.png" alt="스탬프">
+      </div>
+      <div id="wrap3">
+        <button class="finish-btn" @click="changepw_submit">완료</button>
+      </div>
+    </div>
+  </div>
+
+  <!-- 미니 : 회원탈퇴 view -->
+  <div v-if="goodbye_page==true" id="goodbye_page">
+    <div class="modal">
+      <div class="modal_background" v-if="byeemailOpen == true">
+        <div class="modal_box">
+          <h4>이메일이 일치하지 않습니다</h4>
+          <button @click="check">확인</button>
+        </div>
+      </div>
+      <div class="modal_background" v-if="byepwOpen == true">
+        <div class="modal_box">
+          <h4>잘못된 비밀번호입니다</h4>
+          <button @click="check">확인</button>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <div id="wrap1">
+        <i class="material-icons">keyboard_arrow_left</i>
+      </div>
+      <div id="wrap2">
+        <h3>Bye, Bye</h3>
+        <img id="goodbye_img" src="../src/assets/10_goodbye.png" alt="회원탈퇴">
+        <ul>
+          <li>
+            <div class="label-box">
+              <span>이메일을 입력해주세요!</span>
+            </div>
+            <div class="input-box">
+              <input type="email" class="inputText" v-model="bye_email" placeholder="이메일을 입력해주세요!">
+            </div>
+          </li>
+          <li>
+            <div class="label-box">
+              <span>비밀번호를 입력해주세요!</span>
+            </div>
+            <div class="input-box">
+              <input type="password" class="inputText" v-model="bye_pw" placeholder="비밀번호를 입력해주세요!">
+            </div>
+          </li>
+          <li>
+            <div class="label-box">
+              <span>비밀번호를 다시 한 번 입력해주세요!</span>
+            </div>
+            <div class="input-box repw-input">
+              <input type="password" class="inputText" v-model="bye_repw" placeholder="비밀번호를 입력해주세요!" @change="chkRePw">
+              <img src="../src/assets/05_check.png" alt="중복확인" v-if="RePw == true">
+            </div>
+          </li>
+        </ul>
+        <p>confirm</p>
+        <img id="stamp_img" src="../src/assets/02_stamp.png" alt="스탬프">
+      </div>
+      <div id="wrap3">
+        <button class="finish-btn" @click="bye_submit">완료</button>
+      </div>
+    </div>
+  </div>
+  <div v-if="goodbye_finish_page==true" id="goodbye_finish_page">
+    <div class="title">Christmas Q25</div>
+    <div class="subtitle">- 당신의 1년을 정리하는 25개의 질문 -</div>
+    <img src="../src/assets/01_wreath.png">
+    <span>탈퇴가 완료되었습니다<br>이용해주셔서 감사했어요 :)<br>행복한 연말 보내세요!</span>
+    <button class="togo_home" @click="togo_home">홈으로</button>
+    <div class="teamname">teamname</div>
+  </div>
+
+  <!-- 미니 : 설정 view -->
+  <div v-if="setting_page==true" id="setting_page">
+    <div class="modal">
+      <div class="modal_background">
+        <div class="modal_box">
+          <h4 class="changepw-btn" @click="togo_changePw_page">비밀번호 변경</h4>
+          <h4 class="goodbye-btn" @click="togo_goodbye_page">회원 탈퇴</h4>
+          <button @click="check">닫기</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
   <!-- 엘 : 질문리스트 view-->
   <div v-if="Q_list_page==true" id="Q_list_page">
+    <img id="setting" src="../src/assets/09_setting.png" alt="설정" @click="togo_setting_page">
+
     <div class="title">
       <div><span class="userName">{{nickName}}</span>'s</div> 
       <div>Christmas Q25</div>
@@ -246,17 +386,21 @@ export default {
   name: 'App',
   data() {
     return{
-      start_page : false,
+      start_page : true,
       introduction_page : false,
       Q_list_page : true,
       pw_find_page : false,
       signUp_page : false,
       complete_page : false,
       login_page : false,
+      goodbye_page : false,
+      goodbye_finish_page : false,
+      changePw_page : false,
+      setting_page : false,
 
       email : '',
 
-      nickName : 'nickName 데이터',
+      nickName : '',
       질문데이터 : '부여된 랜덤 질문 리스트 데이터',
       ClickButton : false,
       nickOpen: false,
@@ -267,7 +411,20 @@ export default {
       chkEng: /[a-zA-Z]/,
       chkEmail: false,
       emailOpen: false,
-      pwformOpen: false
+      pwformOpen: false,
+      RePw: false,
+      bye_email: '',
+      bye_pw:'',
+      bye_repw:'',
+      byeemailOpen: false,
+      byepwOpen: false,
+      old_pw: '',
+      new_pw: '',
+      newpwOpen: false,
+      oldpwOpen: false,
+      changepwOpen: false,
+      chknewPw: true,
+      
     }
   },
   methods: {
@@ -298,6 +455,25 @@ export default {
     togo_Qlist_page(){
       this.Q_list_page=true;
       this.login_page=false;
+      this.changePw_page=false;
+      this.changepwOpen = false;
+    },
+    togo_home(){
+      this.goodbye_finish_page = false;
+      this.start_page = true;
+    },
+    togo_changePw_page(){
+      this.Q_list_page = false;
+      this.setting_page = false;
+      this.changePw_page = true;
+    },
+    togo_goodbye_page(){
+      this.Q_list_page = false;
+      this.setting_page = false;
+      this.goodbye_page = true;
+    },
+    togo_setting_page(){
+      this.setting_page = true;
     },
 
     random_Q(){
@@ -329,11 +505,47 @@ export default {
         this.signUp_page=false;
       }
     },
+    bye_submit(e){
+      e.preventDefault();
+      // api 받아와서 수정해야함
+      if(this.bye_email == ''){
+        this.byeemailOpen = true;
+        this.byepwOpen = false;
+      }
+      else if(this.bye_pw == ''){
+        this.byepwOpen = true;
+      }
+      else if(this.bye_email !== '' && this.bye_pw !== '' && this.bye_repw !== ''){
+        this.goodbye_page = false;
+        this.goodbye_finish_page = true;
+      }
+    },
+    changepw_submit(e){
+      e.preventDefault();
+      if(this.chknewPw == false){
+        this.newpwOpen = true;
+        this.oldpwOpen = false;
+        this.changepwOpen = false;
+      }
+      // api 받아와서 수정해야함
+      else if(this.old_pw == ''){
+        this.oldpwOpen = true;
+        this.changepwOpen = false;
+      }
+      else if(this.chknewPw == true && this.old_pw !== ''){
+        this.changepwOpen = true;
+      }
+    },
     check(){
       this.nickOpen = false;
       this.pwOpen = false;
       this.emailOpen = false;
       this.pwformOpen = false;
+      this.byeemailOpen = false;
+      this.byepwOpen = false;
+      this.oldpwOpen = false;
+      this.newpwOpen = false;
+      this.setting_page = false;
     },
     chkInput(){
       if (this.password.length < 6){
@@ -348,9 +560,31 @@ export default {
       if(this.password.length > 5 && this.chkNum.test(this.password) && this.chkEng.test(this.password))
         this.chkPw = true;
     },
+    chkInput_new(){
+      if (this.new_pw.length < 6){
+        this.chknewPw = false;
+      }
+      if(!this.chkNum.test(this.new_pw)){
+        this.chknewPw = false;
+      }
+      if(!this.chkEng.test(this.new_pw)){
+        this.chknewPw = false;
+      }
+      if(this.new_pw.length > 5 && this.chkNum.test(this.new_pw) && this.chkEng.test(this.new_pw))
+        this.chknewPw = true;
+    },
     chkOverlap(){
       this.chkEmail = true;
     },
+    chkRePw(){
+      if(this.bye_pw == this.bye_repw){
+        this.RePw = true;
+      }
+      else{
+        this.RePw = false;
+      }
+    },
+    
     // checkInfo(e){
     //   e.preventDefault();
     //   if (this.email != email){
@@ -591,8 +825,15 @@ span {vertical-align: baseline;}
   width: 400px;
   height: 660px;
   background: rgba(217,217,217,0.7);
-  position: fixed;
-  right: 285px;
+  position: absolute;
+  left: -34px;
+}
+#setting_page .modal_background {
+  width: 400px;
+  height: 660px;
+  background: rgba(217,217,217,0.7);
+  position: absolute;
+  left: -200px;
 }
 .modal_background .modal_box {
   width: 223px;
@@ -602,6 +843,14 @@ span {vertical-align: baseline;}
   padding: 16px;
   margin: 246px 68px;
 }
+#setting_page .modal_background .modal_box {
+  width: 223px;
+  height: 110px;
+  background: #F4E7B6;
+  border-radius: 10px;
+  padding: 16px;
+  margin: 62px 0 0 112px;
+}
 .modal_background .modal_box h4 {
   padding: 20px 0 30px 0;
   border-bottom: 0.3px solid #000;
@@ -610,7 +859,28 @@ span {vertical-align: baseline;}
   font-family: 'NanumSquareRound';
 }
 .modal_background.check-email .modal_box h4 {
+  padding: 6px 0 18px 0;
+  border-bottom: 0.3px solid #000;
+  font-size: 16px;
+  font-weight: 800;
+  font-family: 'NanumSquareRound';
+}
+.modal_background.check-oldpw .modal_box h4 {
   padding: 14px 0 18px 0;
+  border-bottom: 0.3px solid #000;
+  font-size: 16px;
+  font-weight: 800;
+  font-family: 'NanumSquareRound';
+}
+#setting_page .modal_background .modal_box .changepw-btn {
+  padding: 4px 0 12px 0;
+  border-bottom: 0.3px solid #000;
+  font-size: 16px;
+  font-weight: 800;
+  font-family: 'NanumSquareRound';
+}
+#setting_page .modal_background .modal_box h4 {
+  padding: 12px 0;
   border-bottom: 0.3px solid #000;
   font-size: 16px;
   font-weight: 800;
@@ -618,6 +888,15 @@ span {vertical-align: baseline;}
 }
 .modal_background .modal_box button {
   padding: 16px 0 0 0;
+  border: none;
+  background: #F4E7B6;
+  color: #920000;
+  font-size: 16px;
+  font-weight: 800;
+  font-family: 'NanumSquareRound';
+}
+#setting_page .modal_background .modal_box button {
+  padding: 12px 0 0 0;
   border: none;
   background: #F4E7B6;
   color: #920000;
@@ -677,8 +956,22 @@ span {vertical-align: baseline;}
   border-bottom: 0.3px solid #000;
   padding: 10px;
 }
+#wrap2 #changePw_img {
+  width: 84px;
+  padding: 38px 0 0 0;
+}
+#wrap2 #goodbye_img {
+  width: 105px;
+  padding: 12px 0 0 0;
+}
 #wrap2 ul {
   padding: 28px 0 0 0;
+}
+#changePw_page #wrap2 ul {
+  padding: 38px 0 0 0;
+}
+#goodbye_page #wrap2 ul {
+  padding: 14px 0 0 0;
 }
 #wrap2 li {
   
@@ -694,6 +987,10 @@ span {vertical-align: baseline;}
   position: relative;
   padding: 18px 0 46px 0;
 }
+#goodbye_page #wrap2 li .input-box {
+  position: relative;
+  padding: 18px 0 28px 0;
+}
 #wrap2 li .input-box.email-input {
   position: relative;
   padding: 18px 0 28px 0;
@@ -701,6 +998,22 @@ span {vertical-align: baseline;}
 #wrap2 li .input-box.pw-input {
   position: relative;
   padding: 18px 0 26px 0;
+}
+#changePw_page #wrap2 li .input-box.newPw-input {
+  position: relative;
+  padding: 18px 0 33px 0;
+}
+#goodbye_page #wrap2 li .input-box.repw-input {
+  position: relative;
+  padding: 18px 0 17px 0;
+}
+#goodbye_page #wrap2 li .input-box.repw-input img {
+  position: absolute;
+  color: #920000;
+  width: 15px;
+  height: 15px;
+  top: 23px;
+  padding: 0 0 0 6px;
 }
 #wrap2 li .input-box .inputText {
   width: 200px;
@@ -753,7 +1066,7 @@ span {vertical-align: baseline;}
   text-align: right;
   padding: 0 35px 0 0;
 }
-#wrap2 > img {
+#wrap2 #stamp_img {
   width: 38px;
   position: absolute;
   right: 27px;
@@ -781,10 +1094,64 @@ span {vertical-align: baseline;}
   background-color: #F4E7B6; */
 }
 
+#goodbye_finish_page {
+
+}
+#goodbye_finish_page .title {
+  font-family: 'Sorts Mill Goudy', serif;
+  color: #FFF500;
+  font-size: 42px;
+  font-weight: 500;
+  padding: 54px 0 10px 0;
+}
+#goodbye_finish_page .subtitle {
+  font-family: 'NanumSquareRound';
+  color: #fff;
+  font-size: 16px;
+  font-weight: 800;
+}
+#goodbye_finish_page img {
+  width: 200px;
+  padding: 57px 80px 30px 80px;
+}
+#goodbye_finish_page span {
+  font-family: 'NanumSquareRound';
+  color: #fff;
+  font-size: 21px;
+  font-weight: 800;
+}
+#goodbye_finish_page .togo_home {
+  display: block;
+  width: 112px;
+  height: 42px;
+  border: none;
+  border-radius: 6px;
+  color: #000;
+  background: #fff;
+  font-family: 'NanumSquareRound';
+  font-size: 16px;
+  font-weight: 800;
+  margin: 33px 124px 14px 124px;
+}
+#goodbye_finish_page .teamname {
+  font-family: 'NanumSquareRound';
+  font-size: 16px;
+  font-weight: 800;
+  color: #fff;
+}
+
 /* 엘 */
 #Q_list_page {
   overflow: scroll;
+  position: relative;
 }
+#Q_list_page #setting {
+  position: absolute;
+  width: 24px;
+  top: 21px;
+  right: 35px;
+}
+
 .title {
   font-family: 'Sorts Mill Goudy', serif;
   color: #FFF500;
